17d724e447eff04cb1ec0b42891e33eff8abf679
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2023-10-02 10:06:53 +0200

    Add solution 1

---
+++ lab01/solution/hello/src/main/java/com/example/micronaut/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.micronaut;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+//tag::class[]
+@Controller // <1>
+public class HelloController {
+
+    @Get("/hello/{name}") // <2>
+    public String sayHello(String name) { // <3>
+        return "Hello " + name; // <4>
+
+    }
+}
+//end::class[]
