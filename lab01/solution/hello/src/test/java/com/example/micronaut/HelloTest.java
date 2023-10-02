281654e9d514383abf3596d7332a82edc7d7bd4c
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2023-10-02 10:41:12 +0200

    Add test

--- lab01/solution/hello/src/test/java/com/example/micronaut/HelloTest.java
+++ lab01/solution/hello/src/test/java/com/example/micronaut/HelloTest.java
@@ -1,5 +1,7 @@
 package com.example.micronaut;
 
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
@@ -7,15 +9,27 @@ import org.junit.jupiter.api.Assertions;
 
 import jakarta.inject.Inject;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @MicronautTest
 class HelloTest {
 
     @Inject
     EmbeddedApplication<?> application;
 
+    @Inject
+    @Client("/") // <1>
+    HttpClient client;
+
     @Test
     void testItWorks() {
         Assertions.assertTrue(application.isRunning());
+    }
+
+    @Test
+    void testSayHello() {
+        String response = client.toBlocking().retrieve("/hello/World");
+        assertEquals("Hello World", response);
     }
 
 }
